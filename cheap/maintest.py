9c44e64da3cdf6ae3c92d547d40ddcce94b692ea
Ved Antani <ved.antani@gmail.com>
2015-01-16 18:05:05 +0530

    Added profilehooks

--- cheap/maintest.py
+++ cheap/maintest.py
@@ -1,45 +1,35 @@
 __author__ = 'vantani'
-from lxml import html
-from lxml.cssselect import CSSSelector
-import requests
 import pylibmc
 from lxml.html import parse,tostring
 from pyquery import PyQuery as pq
-from lxml import etree
-
+from profilehooks import timecall
 
 mc = pylibmc.Client(["127.0.0.1"], binary=True,behaviors={"tcp_nodelay": True,"ketama": True})
+BASE_URL="http://www.flipkart.com/search?q="
 
-def get_page(url):
-    doc = parse(url).getroot()
-    doc.make_links_absolute()
-    return tostring(doc)
-
-def flipkart_book_parser(search_term):
-
+@timecall(immediate=False)
+def get_page(search_term):
     if 'flipkart_test_{0}'.format(search_term) in mc:
         page=mc['flipkart_test_{0}'.format(search_term)]
+        return page
     else:
-        page=get_page('http://www.flipkart.com/search?q={0}'.format(search_term))
-        mc['flipkart_test_{0}'.format(search_term)] =page
+        doc = parse(BASE_URL+search_term).getroot()
+        doc.make_links_absolute()
+        mc['flipkart_test_{0}'.format(search_term)] = tostring(doc)
+        return tostring(doc)
 
-    d = pq(page)
+@timecall(immediate=False)
+def flipkart_book_parser(search_term):
+
+    d = pq(get_page(search_term))
     print d('div.pu-final').text()
     print d('a.lu-title').text()
     print d('div.lu-title-wrapper span.fk-font-11').text()
     print d('div.pu-discount').text()
 
-
-
+@timecall(immediate=False)
 def flipkart_mobile_parser(search_term):
-
-    if 'flipkart_test_{0}'.format(search_term) in mc:
-        page=mc['flipkart_test_{0}'.format(search_term)]
-    else:
-        page=get_page('http://www.flipkart.com/search?q={0}'.format(search_term))
-        mc['flipkart_test_{0}'.format(search_term)] =page
-
-    d = pq(page)
+    d = pq(get_page(search_term))
     print d('div.pu-final').text()
     print d('div.pu-title a').text()
     print d('a.pu-image img').attr('src')
@@ -48,4 +38,5 @@ def flipkart_mobile_parser(search_term):
 
 if __name__ == '__main__':
     flipkart_book_parser('algorithms')
-    #flipkart_mobile_parser('lenovo vibe')
+    print '-' * 10
+    flipkart_mobile_parser('lenovo vibe')
